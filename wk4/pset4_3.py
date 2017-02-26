4b4593f1c979babf9c9449f53148feaa4561f6a7
kristycyw <kristy.chang14@sps.nus.edu.sg>
2017-02-25 19:48:29 -0500

    prob set 4 changes

---
+++ wk4/pset4_3.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import pysam
+import matplotlib.pyplot as plt
+
+bamfile=pysam.AlignmentFile("mapped_181_Sc_BC187.sorted.bam", "rb")
+
+quality=[]
+for read in bamfile.fetch():
+	quality.append(read.mapping_quality)
+plot=plt.hist(quality,bins=50)
+plt.title("Frequency of Mapping Quality")
+plt.xlabel("Mapping Quality")
+plt.ylabel("Frequency")
+plt.savefig("freqmapqual.jpg")
+#plt.show(plot)
+plt.clf()
+
+forrev=[]
+for read1 in bamfile.fetch():
+	forrev.append(read1.is_reverse)
+total=forrev.count(True)+forrev.count(False)
+false=float(forrev.count(False))/total
+true=float(forrev.count(True))/total
+print 'Proportion of reads are on the reverse strand (true) is' , true
+print 'Proportion of reads are on the forward strand (false) is' , false
+
+
+
+
