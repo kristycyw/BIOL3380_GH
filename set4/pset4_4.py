d7ee3f3a27e99220c25ff7f02968d624df0977b1
kristycyw <kristy.chang14@sps.nus.edu.sg>
2017-03-02 21:57:10 -0500

    rest of 4 and 5

--- set4/pset4_4.py
+++ set4/pset4_4.py
@@ -43,22 +43,32 @@ out.close()
 os.remove("fasta_temp.fasta")
 
 bamfile=pysam.AlignmentFile("mapped_181_Sc_BC187.sorted.bam", "rb")
+nonref_out=open('nonref_out.txt',"w")
+#check=open('check.txt',"w")
 
-def nCr(n,r):
+def nCr(n,k):
 	f=math.factorial
-	return f(n)/f(r)/f(n-r)
+	return f(n)/f(k)/f(n-k)
 E=0.01
 for pileupcolumn in bamfile.pileup():
 	#print ("\ncoverage at chrom %s base %s = %s" % (pileupcolumn.reference_name,pileupcolumn.pos, pileupcolumn.n))
 	nonref=0
+	nonref_readlist=[]
 	for pileupread in pileupcolumn.pileups:
+		if pileupcolumn.n > 100:
+			continue
 		if not pileupread.is_del and not pileupread.is_refskip:
 			#query position is None if is_del or is_refskip is set.
 			#print ('\tbase in read %s = %s' % (pileupread.alignment.query_name, pileupread.alignment.query_sequence[pileupread.query_position]))
 			read=pileupread.alignment.query_sequence[pileupread.query_position]
 			if read is not fasta[pileupcolumn.reference_name][0][pileupcolumn.pos]:
 				nonref+=1
+				nonref_readlist.append(read)
 	if nonref is not 0:
+#		check.write("pos= %s \t nonref= %s \n"%(pileupcolumn.n, nonref))
 		gene_likelihood=nCr(pileupcolumn.n,nonref)*(E**nonref)*((1-E)**(pileupcolumn.n-nonref))
-		print ("chrom=%s, base=%s, coverage=%s, gene likelihood=%s"%(pileupcolumn.reference_name,pileupcolumn.pos,pileupcolumn.n,gene_likelihood))		
-#bamfile.close()
+		alt_gene_likelihood=nCr(pileupcolumn.n,nonref)*((1-E)**nonref)*(E**(pileupcolumn.n-nonref))
+		#print("chromosome=%s, position=%s, coverage=%s, gene likelihood=%s"%(pileupcolumn.reference_name,pileupcolumn.pos,pileupcolumn.n,gene_likelihood))	
+		summ_nonref=list(set(nonref_readlist))
+		nonref_out.write("%s \t %s \t %s \t %s \t %s \t %s \n" %(pileupcolumn.reference_name, pileupcolumn.pos, fasta[pileupcolumn.reference_name][0][pileupcolumn.pos], summ_nonref, gene_likelihood, alt_gene_likelihood))
+print "nonref_out.txt contains a 6 column file in the order: chromosome, position, ref allele, alt allele, genotype likelihood given ref, genotype likelihood given alt"
